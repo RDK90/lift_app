efb671efbbb5043ea6845db5d250b9f778a6cfd1
rory.kavanagh <rory.kavanagh@ucdconnect.ie>
2019-06-04 21:30:55 +0100

    Migrate training model to Postgres

--- workouts/models.py
+++ workouts/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Workout(models.Model):
-    #Define choices
-    REP_CAT = ((WARMUP:'Warm up'), (WORK:'Work'), (AMRAP:'Amrap'))
-
+class Training(models.Model):
     #Define data types
     date = models.DateField()
     exercise_category = models.CharField(max_length=10)
@@ -12,4 +9,7 @@ class Workout(models.Model):
     set_number = models.IntegerField()
     reps = models.IntegerField()
     weight = models.FloatField()
-    rep_category = models.CharField(max_length=10, choices=REP_CAT)
+    rep_category = models.CharField(max_length=10)
+
+    def _str_(self):
+        return self.date
