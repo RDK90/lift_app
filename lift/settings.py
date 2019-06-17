b84053c72d1f9ef849a58495dfedb6e07c9693a6
rory.kavanagh <rory.kavanagh@ucdconnect.ie>
2019-06-17 22:38:46 +0100

    Update of allowed hosts

--- lift/settings.py
+++ lift/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dmir#$!=+g!tu1td8_lyq@n1aai2bna8c_19(_+rm^_fk^mbwo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
