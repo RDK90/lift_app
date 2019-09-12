c15335d6ffb734899f02ece2a97c94bbf13e2021
rory.kavanagh <rory.kavanagh@ucdconnect.ie>
2019-09-12 19:38:54 +0100

    Update of unit test strategy

--- workouts/tests/test_views.py
+++ workouts/tests/test_views.py
@@ -1,27 +1,43 @@
-from workouts import views
-from django.test import TestCase
-from workouts.models import Training
 import json
-from rest_framework.test import APIRequestFactory
-from unittest.mock import patch
+from rest_framework import status
+from django.test import TestCase, Client
+from django.urls import reverse
+from workouts.models import Training
+from workouts.serializers import TrainingSerializer
 
-class WorkoutAPITest(TestCase):  
+# initialize the APIClient app
+client = Client()
+
+class WorkoutsTest(TestCase):
+
     def setUp(self):
-        self.factory = APIRequestFactory()
-        self.workout_data = ""
-        with open("workouts/tests/workout_sample.json", "r") as json_file:
-            self.workout_data = json.load(json_file)
+        Training.objects.create(
+            id=2, date="2019-03-25", exercise_category="T1", exercise="Low Bar Squat",
+            set_number=1, reps=8, weight=20, rep_category="Warm up"
+        )
+        Training.objects.create(
+            id=3, date="2019-03-25", exercise_category="T1", exercise="Low Bar Squat",
+            set_number=2, reps=4, weight=70, rep_category="Work"
+        )
+        Training.objects.create(
+            id=4, date="2019-03-25", exercise_category="T1", exercise="Low Bar Squat",
+            set_number=3, reps=4, weight=110, rep_category="Work"
+        )
+        Training.objects.create(
+            id=5, date="2019-03-25", exercise_category="T1", exercise="Low Bar Squat",
+            set_number=4, reps=4, weight=120, rep_category="Work"
+        )
 
-    def test_all_workouts(self):
-        with patch("requests.get") as mock_call_get:
-            mock_call_get.return_value.content = self.workout_data
-            request = self.factory.get('/workouts')
-            response = views.get_all(request)
-            self.assertEqual(response.status_code, 200)
-
-    def test_single_workout(self):
-        with patch("requests.get") as mock_call_get:
-            mock_call_get.return_value.content = self.workout_data
-            request = self.factory.get('/workouts/2')
-            response = views.get_by_id(request, 2)
-            self.assertEqual(response.status_code, 200)
+    def test_get_all_worktouts(self):
+        response = client.get(reverse('workouts:all_workouts'))
+        workout_data = Training.objects.all()
+        serializer = TrainingSerializer(workout_data, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_get_all_worktouts(self):
+        response = client.get(reverse('workouts:id_workouts', kwargs={'id':2}))
+        workout_data = Training.objects.filter(id=2)
+        serializer = TrainingSerializer(workout_data, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
