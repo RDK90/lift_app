04dfe1b3b34cfeb928d43ca0ece3e60c7910cc46
rory.kavanagh <rory.kavanagh@ucdconnect.ie>
2019-09-17 20:46:48 +0100

    Addition of POST workout by id API

--- workouts/views.py
+++ workouts/views.py
@@ -14,9 +14,12 @@ def validate_date(date):
         return True
     except:
         return False
+
+def format_date(workout_id):
+    return "{}-{}-{}".format(workout_id[4:8], workout_id[2:4], workout_id[0:2])
         
 @api_view(['GET'])
-def get_all(request):
+def all_workouts(request):
     if request.method == "GET":
         workouts = Training.objects.all()
         training_serializer = TrainingSerializer(workouts, many=True)
@@ -32,22 +35,41 @@ def get_all(request):
         response_data.pop(0)
         return Response(response_data)
 
-@api_view(['GET'])
-def get_by_id(request, workout_id):
-    date = "{}-{}-{}".format(workout_id[4:8], workout_id[2:4], workout_id[0:2])
-    if not validate_date(date):
-        content = {"Error message": "Invalid date {} found. Correct date format is DDMMYYY".format(date)}
-        return Response(content, status=status.HTTP_400_BAD_REQUEST)
-    workouts = Training.objects.filter(date=date).values()
-    if not workouts:
-        content = {"Error message": "No workouts for date {} found".format(date)}
-        return Response(content, status=status.HTTP_404_NOT_FOUND)
-    else:
-        if request.method == "GET":
+@api_view(['GET', 'POST'])
+def workouts_by_id(request, workout_id):
+    date = format_date(workout_id)
+    if request.method == "GET":
+        if not validate_date(date):
+            content = {"Error message": "Invalid date {} found. Correct date format is DDMMYYY".format(date)}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        workouts = Training.objects.filter(date=date).values()
+        if not workouts:
+            content = {"Error message": "No workouts for date {} found".format(date)}
+            return Response(content, status=status.HTTP_404_NOT_FOUND)
+        else:
             training_serializer = TrainingSerializer(workouts, many=True)
             for workouts in training_serializer.data:
                 workouts.pop("date")
             return Response({"date":date, "workout": training_serializer.data})
+    if request.method == "POST":
+        data = []
+        date = format_date(request.data.get("date"))
+        for post_data in request.data["workout"]:
+            data.append({
+                "date": date,
+                "exercise_category": post_data.get("exercise_category"),
+                "exercise": post_data.get("exercise"),
+                "set_number": post_data.get("set_number"),
+                "reps": post_data.get("reps"),
+                "weight": post_data.get("weight"),
+                "rep_category": post_data.get("rep_category")
+            })
+        training_serializer = TrainingSerializer(data=data, many=True)
+        if training_serializer.is_valid():
+            training_serializer.save()
+            return Response(training_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(training_serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
 
     
 
