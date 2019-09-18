a646fdc141629af07572908a384cdced17d86b11
rory.kavanagh <rory.kavanagh@ucdconnect.ie>
2019-09-18 20:17:26 +0100

    Update of python docker push script to accept environment variables as input parameters

--- build_lift_container.py
+++ build_lift_container.py
@@ -1,13 +1,22 @@
 #Build Lift App Container
 import docker
 from getpass import getpass
+import argparse
+
+my_parser = argparse.ArgumentParser()
+my_parser.add_argument('--repo', action='store', type=str, required=True)
+my_parser.add_argument('--user', action='store', type=str, required=True)
+my_parser.add_argument('--passwd', action='store', type=str, required=True)
+
+args = my_parser.parse_args()
 
 #Define Docker variables
 tag="latest"
-repo=docker_repo
+repo=args.repo
 client = docker.from_env()
 lift_app = client.images.get("liftapp_web")
-client.login(username=docker_username, password=docker_password)
+client.login(username=args.user, password=args.passwd)
 lift_app.tag(repo, tag=tag)
 for line in client.images.push(repo, stream=True, decode=True):
     print(line)
+
