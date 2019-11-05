20aade30050335ef73c75fb153fcc6a9f1c526f7
rory.kavanagh <rory.kavanagh@ucdconnect.ie>
2019-11-05 20:21:42 +0000

    Addition of exercises table in DB

--- workouts/tests/test_models.py
+++ workouts/tests/test_models.py
@@ -1,10 +1,14 @@
 from django.test import TestCase
-from workouts.models import Training
+from workouts.models import Training, Exercises
 from datetime import datetime
 # Create your tests here.
 class ModelsTest(TestCase):
 
-    def test_str_method(self):
+    def test_training_str_method(self):
         training_date = Training(date='25/03/19')
         self.assertEqual(str(training_date), training_date.date)
+
+    def test_exercise_str_method(self):
+        exercise = Exercises(exercise="Bench Press")
+        self.assertEqual(str(exercise), exercise.exercise)
 
