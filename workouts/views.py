ff84fcbf3555f767e24fe5b9023603caf594c7cf
rory.kavanagh <rory.kavanagh@ucdconnect.ie>
2020-03-10 22:42:49 +0000

    Add tests for login method

--- workouts/views.py
+++ workouts/views.py
@@ -148,11 +148,11 @@ def login(request):
     password = request.data.get("password")
     if username is None or password is None:
         return Response({'error': 'Please provide both username and password'},
-                        status=HTTP_400_BAD_REQUEST)
+                        status=status.HTTP_400_BAD_REQUEST)
     user = authenticate(username=username, password=password)
     if not user:
         return Response({'error': 'Invalid Credentials'},
-                        status=HTTP_404_NOT_FOUND)
+                        status=status.HTTP_404_NOT_FOUND)
     token, _ = Token.objects.get_or_create(user=user)
     return Response({'token': token.key},
                     status=status.HTTP_200_OK)
