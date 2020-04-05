661b8c3e66ab1ae13b30342979941516c82215a0
rory.kavanagh <rory.kavanagh@ucdconnect.ie>
2020-04-05 13:09:17 +0100

    Refactor characteristics API from views.py

--- workouts/views.py
+++ workouts/views.py
@@ -35,27 +35,6 @@ def exercises_by_name(request, exercise_name):
     if request.method == "PUT":
         return put_post_exercises_by_name_response(request)
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
-def characteristics_by_date(request, date):
-    date = format_date(date)
-    try:
-        characteristics = Characteristics.objects.filter(date=date).values()
-    except:
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-    if request.method == "GET":
-        if not characteristics:
-            content = {"Error Message: Characteristics for date {} not found".format(date)}
-            return Response(content, status=status.HTTP_404_NOT_FOUND)
-        else:
-            characteristics_serializer = CharacteristicsSerializer(characteristics, many=True)
-            return Response(characteristics_serializer.data, status=status.HTTP_200_OK)
-    elif request.method == 'POST' or request.method == 'PUT':
-        return put_post_characteristics_by_date_response(request)
-    elif request.method == 'DELETE':
-        characteristics = Characteristics.objects.filter(date=date)
-        characteristics.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
 @api_view(['GET'])
 def all_plans(request):
     if request.method == "GET":
