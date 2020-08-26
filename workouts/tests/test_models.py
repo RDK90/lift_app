e33a36fbad234f43934ed106d4995aac2a8dfef8
rory.kavanagh <rory.kavanagh@ucdconnect.ie>
2020-08-26 22:08:58 +0100

    Fixing code coverage with updated training v2 test

--- workouts/tests/test_models.py
+++ workouts/tests/test_models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from workouts.models import Characteristics, Exercises, Plan, Training, Profile
+from workouts.models import Characteristics, Exercises, Plan, Training, Profile, TrainingVersionTwo
 
 
 # Create your tests here.
@@ -38,5 +38,5 @@ class ModelsTest(TestCase):
 
     # v2 Tests
     def test_training_version_two_str_method(self):
-        training_date = Training(date='25/03/19')
+        training_date = TrainingVersionTwo(date='25/03/19')
         self.assertEqual(str(training_date), training_date.date)
