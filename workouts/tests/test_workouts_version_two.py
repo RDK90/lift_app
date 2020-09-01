51e3ec89be6b27e6a95e69bbb7e446a2c826ea74
rory.kavanagh <rory.kavanagh@ucdconnect.ie>
2020-09-01 23:20:22 +0100

    Addition of PUT and POST workouts by date v2 API

--- workouts/tests/test_workouts_version_two.py
+++ workouts/tests/test_workouts_version_two.py
@@ -34,6 +34,49 @@ class TestWorkoutsVersionTwo(TestCase):
             set_number=4, reps=8, weight=50, rep_category="Work"
         )
 
+        self.valid_payload = {
+            "date" : "16092019",
+            "workout": [
+                {
+                    "exercise_category":"T1",
+                    "exercise": "Deadlift",
+                    "set_number": 1,
+                    "reps": 4,
+                    "weight": 70,
+                    "rep_category": "Warm up"
+                },
+                {
+                    "exercise_category":"T1",
+                    "exercise": "Deadlift",
+                    "set_number": 1,
+                    "reps": 4,
+                    "weight": 110,
+                    "rep_category": "Warm up"
+                }
+            ]
+        }
+        self.invalid_payload = {
+            "date" : "999",
+            "workout": [
+                {
+                    "exercise_category":"T1",
+                    "exercise": "Deadlift",
+                    "set_number": 1,
+                    "reps": 4,
+                    "weight": 70,
+                    "rep_category": "Warm up"
+                },
+                {
+                    "exercise_category":"T1",
+                    "exercise": "Deadlift",
+                    "set_number": 1,
+                    "reps": 4,
+                    "weight": 110,
+                    "rep_category": "Warm up"
+                }
+            ]
+        }
+
     def test_get_all_workouts_version_two(self):
         response = self.client.get(reverse('workouts:all_workouts_v2'))
         workout_data = TrainingVersionTwo.objects.all()
@@ -63,6 +106,60 @@ class TestWorkoutsVersionTwo(TestCase):
         self.no_auth_client = APIClient()
         response = self.no_auth_client.get(reverse('workouts:workouts_by_date_v2', kwargs={'date':'25032019'}))
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_post_workout_by_date_version_two(self):
+        response = self.client.post(
+            reverse("workouts:workouts_by_date_v2", kwargs={'date': self.valid_payload["date"]}),
+            data=json.dumps(self.valid_payload),
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_post_invalid_datetype_workout_by_date_version_two(self):
+        response = self.client.post(
+            reverse("workouts:workouts_by_date_v2", kwargs={'date': self.invalid_payload["date"]}),
+            data=json.dumps(self.invalid_payload),
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_post_workouts_no_auth_token_version_two(self):
+        self.no_auth_user = User.objects.create(username="notoken")
+        self.no_auth_client = APIClient()
+        response = self.no_auth_client.post(
+            reverse("workouts:workouts_by_date_v2", kwargs={'date': self.valid_payload["date"]}),
+            data=json.dumps(self.valid_payload),
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_put_workout_by_date_version_two(self):
+        response = self.client.put(
+            reverse("workouts:workouts_by_date_v2", kwargs={"date": self.valid_payload["date"]}),
+            data=json.dumps(self.valid_payload),
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_put_invalid_datetype_workout_by_date_version_two(self):
+        response = self.client.put(
+            reverse("workouts:workouts_by_date_v2", kwargs={"date": self.invalid_payload["date"]}),
+            data=json.dumps(self.invalid_payload),
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_put_workouts_no_auth_token_date_version_two(self):
+        self.no_auth_user = User.objects.create(username="notoken")
+        self.no_auth_client = APIClient()
+        response = self.no_auth_client.put(
+            reverse("workouts:workouts_by_date_v2", kwargs={"date": self.valid_payload["date"]}),
+            data=json.dumps(self.valid_payload),
+            content_type="application/json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
 
     def tearDown(self):
         User.objects.filter(username="nerd").delete()
